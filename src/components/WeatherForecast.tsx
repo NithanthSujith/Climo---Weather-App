02a5f486fe3a402d5f85491574d2621598e3b8c4
Nithanth Sujith <snithanth@gmail.com>
2025-07-16 11:33:55 +0530

    Solve design bugs

--- src/components/WeatherForecast.tsx
+++ src/components/WeatherForecast.tsx
@@ -60,7 +60,7 @@ const WeatherForecast = ({ forecastData }: WeatherForecastProps) => {
                         <p className="font-medium">{format(new Date(day.date * 1000), "EEE, MMM d")}</p>
                         <p className="text-sm text-muted-foreground capitalize">{day.weather.description}</p>
                     </div>
-                    <div className="flex justify-center gap-4 md:gap-1 md:flex-col lg:flex-row lg:gap-4">
+                    <div className="flex justify-center gap-1 flex-col sm:gap-4 sm:flex-row md:gap-1 md:flex-col lg:flex-row lg:gap-4">
                         <span className="flex items-center text-blue-500">
                             <ArrowDown className="mr-1 h-4 w-4" />
                             {formatTemp(day.temp_min)}
@@ -70,7 +70,7 @@ const WeatherForecast = ({ forecastData }: WeatherForecastProps) => {
                             {formatTemp(day.temp_max)}
                         </span>
                     </div>
-                    <div className="flex justify-end gap-4 md:gap-1 md:flex-col  lg:flex-row lg:gap-4">
+                    <div className="flex justify-end gap-1 flex-col sm:gap-4 sm:flex-row md:gap-1 md:flex-col  lg:flex-row lg:gap-4">
                         <span className="flex items-center gap-1 ">
                             <Droplet className="h-4 w-4 text-blue-500" />
                             <span className="text-sm">{day.humidity}%</span>
